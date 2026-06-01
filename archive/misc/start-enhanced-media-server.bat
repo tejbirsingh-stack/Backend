9a85c7cc2e33d6fa0406bc85fb563f8c1914e178
Soham Kashyap <soham-kashyap@cssoftsolutions.com>
2026-06-01 10:47:44 +0530

    New_Structure_1June2026

--- archive/misc/start-enhanced-media-server.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Starting Enhanced Media Server on port 4000...
-echo.
-cd apps\api
-node src\enhanced-media-server.cjs
-pause
--- archive/misc/test-api.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Testing Noah API Server...
-echo.
-
-cd /d "%~dp0apps\api"
-
-echo Starting API on port 4000...
-set PORT=4000
-npx tsx src/simple-api.ts
