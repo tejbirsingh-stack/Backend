e1a91b7232378e82a39d53a479b8ebdfe94562d9
parveen kumar <parveen@csgroupchd.com>
2026-07-28 17:08:05 +0530

    add changes for feature fav

--- apps/api/src/controller/index.js
+++ apps/api/src/controller/index.js
@@ -1,31 +1,17 @@
-const analyticsController = require('./analyticsController');
-const annotationController = require('./annotationController');
-const authController = require('./authController');
-const collectionController = require('./collectionController');
-const compressionController = require('./compressionController');
-const healthController = require('./healthController');
-const mediaController = require('./mediaController');
-const organizationController = require('./organizationsController');
-const realtimeController = require('./realtimeController');
-const userController = require('./userController');
-const roomController = require('./roomController');
-const cronController = require('./cronController');
-const notificationController = require('./notificationController');
-
 module.exports = {
-  ...annotationController,
-  ...authController,
-  ...healthController,
-  ...collectionController,
-  ...compressionController,
-  ...analyticsController,
-  ...organizationController,
-  ...realtimeController,
-  ...userController,
-  ...mediaController,
-  ...roomController,
-  ...cronController,
-  ...notificationController,
+  ...require('./analyticsController'),
+  ...require('./annotationController'),
+  ...require('./authController'),
+  ...require('./collectionController'),
+  ...require('./compressionController'),
+  ...require('./analyticsController'),
+  ...require('./organizationController'),
+  ...require('./realtimeController'),
+  ...require('./userController'),
+  ...require('./mediaController'),
+  ...require('./roomController'),
+  ...require('./cronController'),
+  ...require('./notificationController'),
   ...require('./workSpaceController'),
   ...require('./favoritesController')
 };
