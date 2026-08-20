37f79cbcb5e3fba816d599d6705fd0f19a263e91
parveen kumar <parveen@csgroupchd.com>
2026-08-20 20:30:03 +0530

    aadd changes

--- apps/api/src/utils/prisma.js
+++ apps/api/src/utils/prisma.js
@@ -1,5 +1,7 @@
 const { PrismaClient } = require('@prisma/client');
 
-const prisma = new PrismaClient();
+const globalForPrisma = global;
+const prisma = globalForPrisma.prisma || new PrismaClient();
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
 
 module.exports = prisma;
